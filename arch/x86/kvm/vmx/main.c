b7866e5f7cc5b31d83538562c2f3d61907ef5f5d
Isaku Yamahata <isaku.yamahata@intel.com>
2022-01-19 04:32:38 -0800

    KVM: x86/tdp_mmu: optimize remote tlb flush
    Implement batched TLB shootdown for optimization.  The current
    implementation to zap multiple the EPT entries, looks like as follows.
    
    Loop on GFNs
      1) Zap the EPT entry.  Zero the EPT entry.  PFN is saved on stack.
      2) TDX SEAMCALL TDH.MEM.RANGE.BLOCK with GFN. This corresponds to
         clearing the present bit.
      3) TDH.MEM.TRACK. This corresponds to local tlb flush.
      4) Send IPI to remote vcpus. This corresponds to remote tlb flush.
      5) When destructing VM, TDH.MEM.REMOVE with PFN. There is no
         corresponding to the VMX EPT operation.
      At the last of zapping
      6) kvm_flush_remote_tlbs_with_address(). This flushes shared EPT pointer.
         No operations on Secure EPT.
    
    The new implementation looks like as follows.  The number of TLB shootdown
    is reduced from the number of the EPT entries to zap to one.
    
    Loop on GFNs
      1) - Zap the EPT entry.
         - Clear present/dirty/access bits
         - Keep PFN
         - Set SPTE_PRIVATE_ZAPPED to indicate valid PFN to unlink from Secure
           EPT.
      2) TDX SEAMCALL TDH.MEM.RANGE.BLOCK with GFN.  This corresponds to
         clearing the present bit.
      KVM remote TLB flush callback does,
      3) TDH.MEM.TRACK. corresponds to local tlb flush
      4) Send IPI to remote vcpus. This corresponds to remote tlb flush.
      5) kvm_flush_remote_tlbs_with_address().  This flushes shared EPT
         pointer. No operations on Secure EPT.  When destructing VM, Check if
         SPTE_PRIVATE_ZAPPED and issue TDH.MEM.REMOVE with PFNs.
    
    For traversing private zapped EPT entries, introduce helper function in
    TDP MMU code.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/main.c
+++ arch/x86/kvm/vmx/main.c
@@ -23,8 +23,9 @@ static int vt_max_vcpus(struct kvm *kvm)
 	return kvm->max_vcpus;
 }
 
-#if IS_ENABLED(CONFIG_HYPERV)
+#if IS_ENABLED(CONFIG_HYPERV) || IS_ENABLED(CONFIG_INTEL_TDX_HOST)
 static int vt_flush_remote_tlbs(struct kvm *kvm);
+static int vt_flush_remote_tlbs_range(struct kvm *kvm, gfn_t gfn, gfn_t nr_pages);
 #endif
 
 static void vt_hardware_disable(void)
@@ -55,7 +56,7 @@ static __init int vt_hardware_setup(void)
 		pr_warn_ratelimited("TDX requires mmio caching.  Please enable mmio caching for TDX.\n");
 	}
 
-#if IS_ENABLED(CONFIG_HYPERV)
+#if IS_ENABLED(CONFIG_HYPERV) || IS_ENABLED(CONFIG_INTEL_TDX_HOST)
 	/*
 	 * TDX KVM overrides flush_remote_tlbs method and assumes
 	 * flush_remote_tlbs_range = NULL that falls back to
@@ -75,9 +76,11 @@ static __init int vt_hardware_setup(void)
 	else
 		vt_x86_ops.protected_apic_has_interrupt = NULL;
 
-#if IS_ENABLED(CONFIG_HYPERV)
-	if (enable_tdx)
+#if IS_ENABLED(CONFIG_HYPERV) || IS_ENABLED(CONFIG_INTEL_TDX_HOST)
+	if (enable_tdx) {
 		vt_x86_ops.flush_remote_tlbs = vt_flush_remote_tlbs;
+		vt_x86_ops.flush_remote_tlbs_range = vt_flush_remote_tlbs_range;
+	}
 #endif
 
 	return 0;
@@ -649,7 +652,7 @@ static void vt_flush_tlb_current(struct kvm_vcpu *vcpu)
 	vmx_flush_tlb_current(vcpu);
 }
 
-#if IS_ENABLED(CONFIG_HYPERV)
+#if IS_ENABLED(CONFIG_HYPERV) || IS_ENABLED(CONFIG_INTEL_TDX_HOST)
 static int vt_flush_remote_tlbs(struct kvm *kvm)
 {
 	if (is_td(kvm))
@@ -659,6 +662,15 @@ static int vt_flush_remote_tlbs(struct kvm *kvm)
 	 * fallback to KVM_REQ_TLB_FLUSH.
 	 * See kvm_arch_flush_remote_tlb() and kvm_flush_remote_tlbs().
 	 */
+	return -EOPNOTSUPP;
+}
+
+static int vt_flush_remote_tlbs_range(struct kvm *kvm, gfn_t gfn, gfn_t nr_pages)
+{
+	if (is_td(kvm))
+		return tdx_sept_flush_remote_tlbs_range(kvm, gfn, nr_pages);
+
+	/* fallback to flush_remote_tlbs method */
 	return -EOPNOTSUPP;
 }
 #endif
