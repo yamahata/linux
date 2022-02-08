906c03ae2598e04e95944e8e20a2f6e4aa6120ec
Isaku Yamahata <isaku.yamahata@intel.com>
2022-02-07 18:26:19 -0800

    [REVERTME] debugfs to operation on TDX debug op
    debugfs to manipulate on debug op of debug guild of TDX module.  Without
    this, it's difficult to know what happened to TDX module. For panic of TDX
    module.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/virt/vmx/tdx/tdx.c
+++ arch/x86/virt/vmx/tdx/tdx.c
@@ -1578,3 +1578,14 @@ void __init tdx_init(void)
 
 	check_tdx_erratum();
 }
+
+bool tdx_is_enabled(void)
+{
+	bool r;
+
+	mutex_lock(&tdx_module_lock);
+	r = tdx_module_status == TDX_MODULE_INITIALIZED;
+	mutex_unlock(&tdx_module_lock);
+	return  r;
+}
+EXPORT_SYMBOL(tdx_is_enabled);
