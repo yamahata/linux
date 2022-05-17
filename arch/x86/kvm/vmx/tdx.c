dd0e78f4c21e829c7bfbbfa8a0a25b7b32c7f262
Isaku Yamahata <isaku.yamahata@intel.com>
2022-05-16 17:39:48 -0700

    KVM: TDX: Allow PMU for guest TD
    Now PMU in TD is supported, unblock perfmon for TD.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -3455,16 +3455,6 @@ static int setup_tdparams(struct kvm *kvm, struct td_params *td_params,
 	if (kvm->created_vcpus)
 		return -EBUSY;
 
-	if (init_vm->attributes & TDX_TD_ATTRIBUTE_PERFMON) {
-		/*
-		 * TODO: save/restore PMU related registers around TDENTER.
-		 * Once it's done, remove this guard.
-		 */
-#define MSG_PERFMON	"TD doesn't support perfmon yet. KVM needs to save/restore host perf registers properly.\n"
-		pr_warn(MSG_PERFMON);
-		return -EOPNOTSUPP;
-	}
-
 	td_params->max_vcpus = kvm->max_vcpus;
 	td_params->attributes = init_vm->attributes;
 	/* td_params->exec_controls = TDX_CONTROL_FLAG_NO_RBP_MOD; */
