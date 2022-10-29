d7cd993a643c23730c6a66f7fe5ef0c77c54e824
Isaku Yamahata <isaku.yamahata@intel.com>
2022-10-28 22:15:17 -0700

    KVM: x86/tdp_mmu: Support TDX private mapping for TDP MMU
    Allocate protected page table for private page table, and add hooks to
    operate on protected page table.  This patch adds allocation/free of
    protected page tables and hooks.  When calling hooks to update SPTE entry,
    freeze the entry, call hooks and unfreeze the entry to allow concurrent
    updates on page tables.  Which is the advantage of TDP MMU.  As
    kvm_gfn_shared_mask() returns false always, those hooks aren't called yet
    with this patch.
    
    When the faulting GPA is private, the KVM fault is called private.  When
    resolving private KVM fault, allocate protected page table and call hooks
    to operate on protected page table. On the change of the private PTE entry,
    invoke kvm_x86_ops hook in __handle_changed_spte() to propagate the change
    to protected page table. The following depicts the relationship.
    
      private KVM page fault   |
          |                    |
          V                    |
     private GPA               |     CPU protected EPTP
          |                    |           |
          V                    |           V
     private PT root           |     protected PT root
          |                    |           |
          V                    |           V
       private PT --hook to propagate-->protected PT
          |                    |           |
          \--------------------+------\    |
                               |      |    |
                               |      V    V
                               |    private guest page
                               |
                               |
         non-encrypted memory  |    encrypted memory
                               |
    PT: page table
    
    The existing KVM TDP MMU code uses atomic update of SPTE.  On populating
    the EPT entry, atomically set the entry.  However, it requires TLB
    shootdown to zap SPTE.  To address it, the entry is frozen with the special
    SPTE value that clears the present bit. After the TLB shootdown, the entry
    is set to the eventual value (unfreeze).
    
    For protected page table, hooks are called to update protected page table
    in addition to direct access to the private SPTE. For the zapping case, it
    works to freeze the SPTE. It can call hooks in addition to TLB shootdown.
    For populating the private SPTE entry, there can be a race condition
    without further protection
    
      vcpu 1: populating 2M private SPTE
      vcpu 2: populating 4K private SPTE
      vcpu 2: TDX SEAMCALL to update 4K protected SPTE => error
      vcpu 1: TDX SEAMCALL to update 2M protected SPTE
    
    To avoid the race, the frozen SPTE is utilized.  Instead of atomic update
    of the private entry, freeze the entry, call the hook that update protected
    SPTE, set the entry to the final value.
    
    Support 4K page only at this stage.  2M page support can be done in future
    patches.
    
    Co-developed-by: Kai Huang <kai.huang@intel.com>
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    
    ---
    v19:
    - drop CONFIG_KVM_MMU_PRIVATE
    
    v18:
    - Rename freezed => frozen
    
    v14 -> v15:
    - Refined is_private condition check in kvm_tdp_mmu_map().
      Add kvm_gfn_shared_mask() check.
    - catch up for struct kvm_range change

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -3741,8 +3741,15 @@ static int mmu_alloc_direct_roots(struct kvm_vcpu *vcpu)
 	unsigned i;
 	int r;
 
-	if (tdp_mmu_enabled)
-		return kvm_tdp_mmu_alloc_root(vcpu);
+	if (tdp_mmu_enabled) {
+		if (kvm_gfn_shared_mask(vcpu->kvm) &&
+		    !VALID_PAGE(mmu->private_root_hpa)) {
+			r = kvm_tdp_mmu_alloc_root(vcpu, true);
+			if (r)
+				return r;
+		}
+		return kvm_tdp_mmu_alloc_root(vcpu, false);
+	}
 
 	write_lock(&vcpu->kvm->mmu_lock);
 	r = make_mmu_pages_available(vcpu);
@@ -4775,7 +4782,7 @@ int kvm_tdp_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 	if (kvm_mmu_honors_guest_mtrrs(vcpu->kvm)) {
 		for ( ; fault->max_level > PG_LEVEL_4K; --fault->max_level) {
 			int page_num = KVM_PAGES_PER_HPAGE(fault->max_level);
-			gfn_t base = gfn_round_for_level(fault->gfn,
+			gfn_t base = gfn_round_for_level(gpa_to_gfn(fault->addr),
 							 fault->max_level);
 
 			if (kvm_mtrr_check_gfn_range_consistency(vcpu, base, page_num))
@@ -6306,6 +6313,7 @@ static int __kvm_mmu_create(struct kvm_vcpu *vcpu, struct kvm_mmu *mmu)
 
 	mmu->root.hpa = INVALID_PAGE;
 	mmu->root.pgd = 0;
+	mmu->private_root_hpa = INVALID_PAGE;
 	for (i = 0; i < KVM_MMU_NUM_PREV_ROOTS; i++)
 		mmu->prev_roots[i] = KVM_MMU_ROOT_INFO_INVALID;
 
@@ -7343,6 +7351,12 @@ out:
 void kvm_mmu_destroy(struct kvm_vcpu *vcpu)
 {
 	kvm_mmu_unload(vcpu);
+	if (tdp_mmu_enabled) {
+		read_lock(&vcpu->kvm->mmu_lock);
+		mmu_free_root_page(vcpu->kvm, &vcpu->arch.mmu->private_root_hpa,
+				NULL);
+		read_unlock(&vcpu->kvm->mmu_lock);
+	}
 	free_mmu_pages(&vcpu->arch.root_mmu);
 	free_mmu_pages(&vcpu->arch.guest_mmu);
 	mmu_free_memory_caches(vcpu);
