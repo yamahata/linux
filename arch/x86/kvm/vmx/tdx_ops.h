bcda8df1f03efb8b062b22d3c5f7e2cef1fdd0ba
Isaku Yamahata <isaku.yamahata@intel.com>
2022-11-15 04:12:16 -0800

    [REVERTME] KVM: TDX, x86/tdp_mmu: Protect Secure-EPT page
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/tdx_ops.h
+++ arch/x86/kvm/vmx/tdx_ops.h
@@ -168,9 +168,13 @@ static inline u64 tdh_mem_sept_add(hpa_t tdr, gpa_t gpa, int level, hpa_t page,
 		.rdx = tdr,
 		.r8 = page,
 	};
+	u64 r;
 
 	clflush_cache_range(__va(page), PAGE_SIZE);
-	return tdx_seamcall_sept(TDH_MEM_SEPT_ADD, &in, out);
+	r = tdx_seamcall_sept(TDH_MEM_SEPT_ADD, &in, out);
+	if (!r)
+		tdx_set_page_np(page);
+	return r;
 }
 
 static inline u64 tdh_mem_sept_rd(hpa_t tdr, gpa_t gpa, int level,
