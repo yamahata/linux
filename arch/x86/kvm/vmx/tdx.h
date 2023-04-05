7e9f0f0b95ef239ded2d3484a8e45b0539bf060a
Isaku Yamahata <isaku.yamahata@intel.com>
2023-04-05 11:59:17 -0700

    KVM: TDX: Do TDX specific vcpu initialization
    TD guest vcpu needs TDX specific initialization before running.  Repurpose
    KVM_MEMORY_ENCRYPT_OP to vcpu-scope, add a new sub-command
    KVM_TDX_INIT_VCPU, and implement the callback for it.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    ---
    v20:
    - Support FEATURES0_TOPOLOGY_ENUM
    - Update for the wrapper functions for SEAMCALLs. (Sean)
    - Remove WARN_ON_ONCE() in tdx_vcpu_free().
      WARN_ON_ONCE(vcpu->cpu != -1), WARN_ON_ONCE(tdx->tdvpx_pa),
      WARN_ON_ONCE(tdx->tdvpr_pa)
    - Remove KVM_BUG_ON() in tdx_vcpu_reset().
    
    v18:
    - Use tdh_sys_rd() instead of struct tdsysinfo_struct.
    - Rename tdx_reclaim_td_page() => tdx_reclaim_control_page()
    - Remove the change of tools/arch/x86/include/uapi/asm/kvm.h.

--- arch/x86/kvm/vmx/tdx.h
+++ arch/x86/kvm/vmx/tdx.h
@@ -16,6 +16,8 @@ struct kvm_tdx {
 	u64 xfam;
 	int hkid;
 
+	bool finalized;
+
 	u64 tsc_offset;
 };
 
@@ -23,6 +25,10 @@ struct vcpu_tdx {
 	struct kvm_vcpu	vcpu;
 
 	unsigned long tdvpr_pa;
+	unsigned long *tdvpx_pa;
+	bool td_vcpu_created;
+
+	bool initialized;
 
 	/*
 	 * Dummy to make pmu_intel not corrupt memory.
