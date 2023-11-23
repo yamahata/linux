f96fd006f319a5a34e47907cd5d1152fbab65cd5
Isaku Yamahata <isaku.yamahata@intel.com>
2023-11-23 14:17:50 -0800

    KVM: x86/mmu: push down mmu lock to kvm_tdp_mmu_zap_all()
    For refactoring kvm_tdp_mmu_zap_all() with locking change, push down
    mmu_lock from kvm_mmu_zap_all() to kvm_tdp_mmu_zap_all().
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -7419,9 +7419,7 @@ static void kvm_mmu_zap_all(struct kvm *kvm)
 		return;
 	}
 
-	write_lock(&kvm->mmu_lock);
 	kvm_tdp_mmu_zap_all(kvm);
-	write_unlock(&kvm->mmu_lock);
 }
 
 void kvm_arch_flush_shadow_all(struct kvm *kvm)
