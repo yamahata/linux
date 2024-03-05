d67c6914afbeba198a41960ffd78d89c9634f497
Isaku Yamahata <isaku.yamahata@intel.com>
2024-03-04 21:40:34 -0800

    KVM: guest_memfd: Fix "[PATCH 19/21] KVM: guest_memfd: add API to undo kvm_gmem_get_uninit_pfn"
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -2489,7 +2489,7 @@ static inline int kvm_gmem_get_uninit_pfn(struct kvm *kvm,
 static inline int kvm_gmem_undo_get_pfn(struct kvm *kvm,
 				        struct kvm_memory_slot *slot, gfn_t gfn,
 				        int order)
-{}
+{ return -EIO; }
 static inline bool kvm_gmem_mapping(const struct address_space *mapping)
 {
 	return false;
