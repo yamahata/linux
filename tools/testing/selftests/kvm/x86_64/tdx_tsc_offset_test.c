c8dd55c55b3631d6e1f47300ce23d4b64af3d621
Isaku Yamahata <isaku.yamahata@intel.com>
2024-10-01 14:14:22 -0700

    NOTFORMERGE: KVM: TDX: Make tdx_tsc_offset_test do the right comparison
    Compare the vCPU TSC offset with TDX TSC offset of VMCS metadata.
    
    This patch is to demonstrate what the right test should be.  Because it
    requires enhancing the KVM vCPU device attribute API, this is not for
    merge, but to demonstrate the patch is tested.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- tools/testing/selftests/kvm/x86_64/tdx_tsc_offset_test.c
+++ tools/testing/selftests/kvm/x86_64/tdx_tsc_offset_test.c
@@ -8,57 +8,39 @@
 
 void guest(void)
 {
-	/* nothing */
+	tdx_test_success();
 }
 
-/*
- * TODO: Get TD_TDCS_EXEC_TSC_OFFSET td metadata.
- * We should check if the vCPU TSC offset matches with the one the TDX module
- * has for the vCPU.  But it's difficult to get the value in the TDX module.
- *
- * KVM maintains TSC offset per vCPU.  By relying on it and tsc=unstable,
- * check if TSC offset for two vCPU are same instead.
- */
 int verify_tsc_offset(void)
 {
-	uint64_t tsc_offset[2], tsc_offset_after[2];
-	struct kvm_vcpu *vcpu[2];
+	uint64_t vmcs_tsc_offset, tsc_offset, tsc_offset_after;
+	struct kvm_vcpu *vcpu;
 	struct kvm_vm *vm;
 
 	vm = td_create();
 	td_initialize(vm, VM_MEM_SRC_ANONYMOUS, 0);
 
-	vcpu[0] = td_vcpu_add(vm, 0, NULL);
-	TEST_ASSERT(vcpu[0], "td_vcpu_add()");
-	vcpu[1] = td_vcpu_add(vm, 1, NULL);
-	TEST_ASSERT(vcpu[1], "td_vcpu_add()");
+	vcpu = td_vcpu_add(vm, 0, guest);
+	TEST_ASSERT(vcpu, "td_vcpu_add()");
 
 	td_finalize(vm);
 
 	printf("Verifying TSC offset for TDX\n");
 
-	vcpu_device_attr_get(vcpu[0], KVM_VCPU_TSC_CTRL,
-			     KVM_VCPU_TSC_OFFSET, &tsc_offset[0]);
-	vcpu_device_attr_get(vcpu[0], KVM_VCPU_TSC_CTRL,
-			     KVM_VCPU_TSC_OFFSET, &tsc_offset[1]);
-	TEST_ASSERT(tsc_offset[0] == tsc_offset[1], "tsc offset doesn't match");
-	/*
-	 * TEST_ASSERT(tsc_offset[0] == TD_TDCS_EXEC_TSC_OFFSET td metadata,
-	 *             "tsc offset doesn't match");
-	 */
+#define MD_TD_VMCS_TSC_OFFSET	0x0024000300002010ULL
+	vcpu_device_attr_get(vcpu, KVM_VCPU_TDX_MD_CTRL, MD_TD_VMCS_TSC_OFFSET,
+			     &vmcs_tsc_offset);
 
-	td_vcpu_run(vcpu[0]);
-	td_vcpu_run(vcpu[1]);
-
-
-	vcpu_device_attr_get(vcpu[0], KVM_VCPU_TSC_CTRL,
-			     KVM_VCPU_TSC_OFFSET, &tsc_offset_after[0]);
-	vcpu_device_attr_get(vcpu[1], KVM_VCPU_TSC_CTRL,
-			     KVM_VCPU_TSC_OFFSET, &tsc_offset_after[1]);
-
-	TEST_ASSERT(tsc_offset_after[0] == tsc_offset[0],
+	vcpu_device_attr_get(vcpu, KVM_VCPU_TSC_CTRL,
+			     KVM_VCPU_TSC_OFFSET, &tsc_offset);
+	TEST_ASSERT(tsc_offset == vmcs_tsc_offset,
 		    "tsc_offset doesn't match");
-	TEST_ASSERT(tsc_offset_after[1] == tsc_offset[1],
+
+	td_vcpu_run(vcpu);
+	TDX_TEST_ASSERT_SUCCESS(vcpu);
+	vcpu_device_attr_get(vcpu, KVM_VCPU_TSC_CTRL,
+			     KVM_VCPU_TSC_OFFSET, &tsc_offset_after);
+	TEST_ASSERT(tsc_offset_after == vmcs_tsc_offset,
 		    "tsc_offset doesn't match");
 
 	printf("\t ... PASSED\n");
@@ -70,28 +52,11 @@ int verify_tsc_offset(void)
 
 int main(int argc, char **argv)
 {
-	char cmdline[4096];
-	ssize_t ret;
-	int fd;
-
 	if (!is_tdx_enabled()) {
 		printf("TDX is not supported by the KVM\n"
 		       "Skipping the TDX tests.\n");
 		return 0;
 	}
-
-	/*
-	 * this should be removed once we can retrieve TD_TDCS_EXEC_TSC_OFFSET
-	 * metadata.
-	 *
-	 * This is for two vCPU can have different TSC OFFSET when KVM
-	 * calculates tsc offset per-vCPU.
-	 */
-	fd = open_path_or_exit("/proc/cmdline", O_RDONLY);
-	ret = read(fd, cmdline, sizeof(cmdline));
-	TEST_ASSERT(ret > 0, "failed to read /proc/cmdline");
-	TEST_REQUIRE(strstr(cmdline, "tsc=unstable"));
-	close(fd);
 
 	return verify_tsc_offset();
 }
